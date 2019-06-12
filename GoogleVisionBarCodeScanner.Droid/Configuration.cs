98a720806bd5728bf8cfd4df041858d849c7fd58
jimmypun <jimmypun@nwconstruction.com.hk>
2019-06-12 11:52:50 +0800

    first commit

---
+++ GoogleVisionBarCodeScanner.Droid/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GoogleVisionBarCodeScanner.Droid
+{
+    public static class Configuration
+    {
+        public static string Title = "掃瞄QRCode";
+        public static string FlashlightMessage = "Flashlight";
+        public static string ScanningDescription = "請掃瞄QRCode";
+        public static Action<string> ScannedQRCode;
+        public static Android.Gms.Vision.Barcodes.BarcodeFormat BarcodeFormats = Android.Gms.Vision.Barcodes.BarcodeFormat.QrCode;
+        
+    }
+}
