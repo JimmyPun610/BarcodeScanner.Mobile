eec4e5d2954a074e2393bb0aacab35c4329e3532
kenyd <chitrung09t2@gmail.com>
2020-01-14 11:58:41 +0700

    [iOS] Optimize camera preview

--- GoogleVisionBarCodeScanner.iOS/RendererInitializar.cs
+++ GoogleVisionBarCodeScanner.iOS/RendererInitializar.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Foundation;
-using UIKit;
-
-namespace GoogleVisionBarCodeScanner.iOS
+﻿namespace GoogleVisionBarCodeScanner.iOS
 {
     public class Initializer
     {
