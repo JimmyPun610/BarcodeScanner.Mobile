9135c25d4a5d47222fad39730b3d8ca446792721
Jimmy Pun <11005408@life.hkbu.edu.hk>
2024-05-27 16:49:20 +1000

    Update appcenter-post-clone.sh

--- SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
+++ SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
@@ -6,6 +6,5 @@ echo "##[warning][Post-clone Action] - Lets do some Post clone transformations..
 ROOT_FOLDER=$APPCENTER_SOURCE_DIRECTORY/
 
 dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/BarcodeScanner.Mobile.Maui/BarcodeScanner.Mobile.Maui.csproj
-dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/OCR.Mobile.Maui/OCR.Mobile.Maui.csproj
 dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/SampleApp.Maui/SampleApp.Maui.csproj
 dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/SampleApp.XF/SampleApp.XF.Android/SampleApp.XF.Android.csproj
