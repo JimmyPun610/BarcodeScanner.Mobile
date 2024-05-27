cf846292db8dbf65733d7a7312a28fad2de4b4cd
Jimmy Pun <11005408@life.hkbu.edu.hk>
2024-05-27 15:37:28 +1000

    Update appcenter-post-clone.sh

--- SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
+++ SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
@@ -3,7 +3,7 @@ echo "##[warning][Post-clone Action] - Lets do some Post clone transformations..
 # Calling main script in solution root
 # This post clone script is to fix issue on XCode 15
 # https://github.com/microsoft/appcenter/issues/2610#issuecomment-1910734646
-ROOT_FOLDER=$APPCENTER_SOURCE_DIRECTORY
+ROOT_FOLDER=$APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile
 
 dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/BarcodeScanner.Mobile.Maui/BarcodeScanner.Mobile.Maui.csproj
 dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/OCR.Mobile.Maui/OCR.Mobile.Maui.csproj
