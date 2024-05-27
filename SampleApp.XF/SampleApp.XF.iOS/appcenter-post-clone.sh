1896c61491261510e33c22d8918835ea83af3845
Jimmy Pun <11005408@life.hkbu.edu.hk>
2024-05-27 16:35:18 +1000

    Update appcenter-post-clone.sh

--- SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
+++ SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
@@ -9,6 +9,8 @@ echo $APPCENTER_SOURCE_DIRECTORY/SampleApp.Maui/SampleApp.Maui.csproj
 
 echo $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln
 
+dotnet sln "$APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln" "list"
+
 dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove $APPCENTER_SOURCE_DIRECTORY/SampleApp.XF/SampleApp.XF.Android/SampleApp.XF.Android.csproj
 dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.Maui/BarcodeScanner.Mobile.Maui.csproj
 dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove $APPCENTER_SOURCE_DIRECTORY/SampleApp.Maui/SampleApp.Maui.csproj
