29e7d319ae1393a0e7964676828a872d14e73360
Jimmy Pun <11005408@life.hkbu.edu.hk>
2024-05-27 16:54:18 +1000

    Update appcenter-post-clone.sh

--- SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
+++ SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
@@ -3,8 +3,7 @@ echo "##[warning][Post-clone Action] - Lets do some Post clone transformations..
 # Calling main script in solution root
 # This post clone script is to fix issue on XCode 15
 # https://github.com/microsoft/appcenter/issues/2610#issuecomment-1910734646
-ROOT_FOLDER=$APPCENTER_SOURCE_DIRECTORY
-ls -1
-dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/BarcodeScanner.Mobile.Maui/BarcodeScanner.Mobile.Maui.csproj
-dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/SampleApp.Maui/SampleApp.Maui.csproj
-dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/SampleApp.XF/SampleApp.XF.Android/SampleApp.XF.Android.csproj
+
+dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove ../../BarcodeScanner.Mobile.Maui/BarcodeScanner.Mobile.Maui.csproj
+dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove ../../SampleApp.Maui/SampleApp.Maui.csproj
+dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove ../SampleApp.XF.Android/SampleApp.XF.Android.csproj
