609abf916b10fb6e090ff629fa3e0931ae740b54
Jimmy Pun <11005408@life.hkbu.edu.hk>
2024-05-27 15:39:22 +1000

    Update appcenter-post-clone.sh

--- SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
+++ SampleApp.XF/SampleApp.XF.iOS/appcenter-post-clone.sh
@@ -3,9 +3,8 @@ echo "##[warning][Post-clone Action] - Lets do some Post clone transformations..
 # Calling main script in solution root
 # This post clone script is to fix issue on XCode 15
 # https://github.com/microsoft/appcenter/issues/2610#issuecomment-1910734646
-ROOT_FOLDER=$APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile
 
-dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/BarcodeScanner.Mobile.Maui/BarcodeScanner.Mobile.Maui.csproj
-dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/OCR.Mobile.Maui/OCR.Mobile.Maui.csproj
-dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/SampleApp.Maui/SampleApp.Maui.csproj
-dotnet sln $ROOT_FOLDER/BarcodeScanner.Mobile.sln remove $ROOT_FOLDER/SampleApp.XF/SampleApp.XF.Android/SampleApp.XF.Android.cspro
+dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.Maui/BarcodeScanner.Mobile.Maui.csproj
+dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove $APPCENTER_SOURCE_DIRECTORY/OCR.Mobile.Maui/OCR.Mobile.Maui.csproj
+dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove $APPCENTER_SOURCE_DIRECTORY/SampleApp.Maui/SampleApp.Maui.csproj
+dotnet sln $APPCENTER_SOURCE_DIRECTORY/BarcodeScanner.Mobile.sln remove $APPCENTER_SOURCE_DIRECTORY/SampleApp.XF/SampleApp.XF.Android/SampleApp.XF.Android.cspro
